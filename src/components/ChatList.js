c5212ff4a7f870caf005ba350620149eec3af237
akash kamat <akash.kamat.10@gmail.com>
2022-06-23 13:42:38 +0530

    added skeleton loading

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -3,37 +3,52 @@ import { ref, onValue, query, orderByChild } from "firebase/database";
 import { useState, useEffect } from 'react'
 import Chat from './Chat';
 import './css/ChatList.css'
+import Loading from './Loading';
 
-export default function ChatList({ db, user }) {
+export default function ChatList({ db, user, loading, setLoading }) {
     const [chats, setChats] = useState([]);
     useEffect(() => {
+        setLoading(true)
         onValue(ref(db, "messages/"), (snapshot) => {
             const data = [];
             snapshot.forEach(e => {
                 data.push(e.val())
             })
             setChats(data)
+            setLoading(false)
 
 
         })
     }, [])
-    return (
-        <div className='all-chats' id='allChats'>
-            {
-                chats.map((e, i) => {
-                    if (e.uid == user.uid) {
+    if (loading) {
+        return (
+            <div className='all-chats' id='allChats'>
+                <Loading direction={"left"} />
+                <Loading direction={"left"} />
+                <Loading direction={"right"} />
 
-                        return <Chat data={e} user={user} key={i} loggedIn={true} />
-                    }
-                    else if (e.uid == "wv7fwmqvtITT5K2UwC8FPp1ckgG2") {
-                        return <Chat data={e} user={user} key={i} admin={true} />
-                    }
-                    else {
-                        return <Chat data={e} user={user} key={i} loggedIn={false} />
-                    }
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='all-chats' id='allChats'>
+                {
+                    chats.map((e, i) => {
+                        if (e.uid == user.uid) {
 
-                })
-            }
-        </div>
-    )
+                            return <Chat data={e} user={user} key={i} loggedIn={true} />
+                        }
+                        else if (e.uid == "wv7fwmqvtITT5K2UwC8FPp1ckgG2") {
+                            return <Chat data={e} user={user} key={i} admin={true} />
+                        }
+                        else {
+                            return <Chat data={e} user={user} key={i} loggedIn={false} />
+                        }
+
+                    })
+                }
+            </div>
+        )
+    }
 }
